2b5e4c3a4586e8e5e29f0e9102f8382bddf7e94f
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 13:11:22 +0900

    mix new image_experiment

---
+++ lib/image_experiment.ex
@@ -0,0 +1,18 @@
+defmodule ImageExperiment do
+  @moduledoc """
+  Documentation for `ImageExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImageExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/image_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule ImageExperimentTest do
+  use ExUnit.Case
+  doctest ImageExperiment
+
+  test "greets the world" do
+    assert ImageExperiment.hello() == :world
+  end
+end
