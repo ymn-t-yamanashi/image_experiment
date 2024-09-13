eccc0e35ab86421f023fd0d35c0e574014e877f1
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 13:39:18 +0900

    サンプル作成

--- lib/image_experiment.ex
+++ lib/image_experiment.ex
@@ -2,6 +2,7 @@ defmodule ImageExperiment do
   @moduledoc """
   Documentation for `ImageExperiment`.
   """
+  @tmp_image_path "/dev/shm/cv.png"
 
   @doc """
   Hello world.
@@ -13,6 +14,22 @@ defmodule ImageExperiment do
 
   """
   def hello do
+    Image.open!("test.png")
+    |> lines(1..1000//5 |> Enum.to_list())
+    |> display_image()
+
     :world
+  end
+
+  def lines(image, []), do: image
+  def lines(image, [x | range]), do: line(image, x) |> lines(range)
+
+  def line(image, x) do
+    Image.Draw.line!(image, 0, 0, x, 450, color: "#0000ff")
+  end
+
+  def display_image(image) do
+    Image.write(image, @tmp_image_path)
+    System.cmd("eog", [@tmp_image_path])
   end
 end
--- test/image_experiment_test.exs
+++ test/image_experiment_test.exs
@@ -1,8 +1,4 @@
 defmodule ImageExperimentTest do
   use ExUnit.Case
   doctest ImageExperiment
-
-  test "greets the world" do
-    assert ImageExperiment.hello() == :world
-  end
 end
